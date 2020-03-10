4341c988b0f6f110c73bd63ad8308769890c59e7
Lord Sayur <umar_317@hotmail.com>
2020-03-10 23:22:40 +0800

    92e73e7

---
+++ precache-manifest.88af4a28e17591187349c672803b5eaa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c68c7d8f37269db717e",
+    "url": "/css/chunk-vendors.06d6f024.css"
+  },
+  {
+    "revision": "07d1e22e8cddd99a53037dc510221b7b",
+    "url": "/img/logo.07d1e22e.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4937a4a7f5b1ead6416d3e3399e5389f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1df2a6b53dfe6887136a",
+    "url": "/js/about.822b71d9.js"
+  },
+  {
+    "revision": "c5ac104a13c3421be6b2",
+    "url": "/js/app.da024a21.js"
+  },
+  {
+    "revision": "1c68c7d8f37269db717e",
+    "url": "/js/chunk-vendors.3f1116d8.js"
+  },
+  {
+    "revision": "340dd40138d343868375b68479751fee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
